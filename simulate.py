99b1e10bdbb330a85007232c13fca39d2991c169
theogguu <38506698+theogguu@users.noreply.github.com>
2023-05-09 14:06:50 -0500

    added box

--- simulate.py
+++ simulate.py
@@ -1,3 +1,15 @@
 import pybullet as p
+import time 
 physicsClient = p.connect(p.GUI)
+
+# Disables sidebars on the simulation
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+# loads sdf
+p.loadSDF("box.sdf")
+
+for n in range(1500):
+    p.stepSimulation()
+    time.sleep(1/60)
+
 p.disconnect()
